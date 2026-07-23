95f117de19a3cc6f1d70c7ff0943908372def56a
Likhita Reddy Duggempudi <likhita1233@gmail.com>
2026-07-23 22:33:29 +0530

    Time: 1 ms (51.02%), Space: 125.6 MB (10.2%) - LeetHub

--- 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.java
+++ 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.java
@@ -2,14 +2,8 @@ class Solution {
     public int uniqueXorTriplets(int[] nums) {
         int n=nums.length;int xor;
         int k=(int)(Math.log(n)/Math.log(2));
-        if(n==1){
-            xor=1;
-        }
-        else if(n==2){
-            xor=2;
-        }
-        else if(n==3){
-            xor=4;
+        if(n<=2){
+            xor=n;
         }
         else{
             xor=(int)Math.pow(2,k+1);
