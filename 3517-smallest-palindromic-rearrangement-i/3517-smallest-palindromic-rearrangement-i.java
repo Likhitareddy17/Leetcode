7d0b26523913ae01ee8c40b1f5469e31574ba5ea
Likhita Reddy Duggempudi <likhita1233@gmail.com>
2026-07-29 23:17:49 +0530

    Time: 29 ms (72.8%), Space: 48.5 MB (10%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
@@ -1,25 +1,24 @@
+import java.util.Arrays;
+
 class Solution {
     public String smallestPalindrome(String s) {
+        int n = s.length();
+        int halfLen = n / 2;
 
-        int[] freq = new int[26];
+        // 1. Extract the left half
+        char[] leftArray = s.substring(0, halfLen).toCharArray();
 
-        for (char c : s.toCharArray())
-            freq[c - 'a']++;
+        // 2. Sort the left half to make it lexicographically smallest
+        Arrays.sort(leftArray);
+        String left = new String(leftArray);
 
-        StringBuilder left = new StringBuilder();
-        String middle = "";
+        // 3. Extract middle character (if length is odd)
+        String middle = (n % 2 == 1) ? String.valueOf(s.charAt(halfLen)) : "";
 
-        for (int i = 0; i < 26; i++) {
-
-            for (int j = 0; j < freq[i] / 2; j++)
-                left.append((char) ('a' + i));
-
-            if (freq[i] % 2 == 1)
-                middle = String.valueOf((char) ('a' + i));
-        }
-
+        // 4. Create right half by reversing the sorted left half
         String right = new StringBuilder(left).reverse().toString();
 
-        return left.toString() + middle + right;
+        // 5. Assemble and return
+        return left + middle + right;
     }
 }
