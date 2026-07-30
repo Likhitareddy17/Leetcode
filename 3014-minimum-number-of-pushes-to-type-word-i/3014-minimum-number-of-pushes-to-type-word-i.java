f2f361d51a0d3cd5fbed647ab5b53d1c02b6c98f
Likhita Reddy Duggempudi <likhita1233@gmail.com>
2026-07-30 22:08:50 +0530

    Time: 0 ms (100%), Space: 43.4 MB (16.51%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -10,9 +10,9 @@ class Solution {
         if(n>16 && n<=24){
             push=(n-8)*3;
         }
-        if(n==25) push= 52;
-        if(n==26) push= 56;
-        if(n==27) push =60;
+        if(n>24){
+            push=48+(n-24)*4;
+        }
         return push;
     }
 }
